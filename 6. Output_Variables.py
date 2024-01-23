646be9320395f254e2f7c738c5eda914078cf44a
ITUNLEASH <157260465+ITUNLEASH@users.noreply.github.com>
2024-01-23 08:55:00 -0700

    Delete 6. Output_Variables.py

--- 6. Output_Variables.py
+++
@@ -1,9 +0,0 @@
-a = "Are"
-b = "You"
-c = "Who"
-d = "Wanna"
-e = "Be?"
-print(a + b + c + d + e)
-#Notice Line 6. When using the + symbol, the variables are printing everhting together in one word.
-#To separate the each variable, meaning each word of the sentence a comma is required. 
-#For example like this print(a, b, c, d, e)
